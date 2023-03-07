13255ba40a8e5676f589ace1ab6e43b397fe30b0
jeminrudani <rudanijemin@gmail.com>
2023-03-07 04:01:09 -0800

    cart and cart item

--- src/pages/productDetail/ProductDetails.js
+++ src/pages/productDetail/ProductDetails.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useParams } from "react-router";
 import dummyImg from "../../assets/naruto.jpeg";
 import './ProductDetails.scss'
 
 function ProductDetails() {
+    const params = useParams();
+
+    console.log('params', params);
   return (
 <div className="ProductDetail">
             <div className="container">
                 <div className="product-layout">
-                    <div className="product-img center">
+                    <div className="product-img     ">
                         <img src={dummyImg} alt="product img" />
                     </div>
                     <div className="product-info">
